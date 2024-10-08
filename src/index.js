3fe1a27456ccc78164df5327412c6fdf791247e8
chaconvargas21 <chaconvargas21@gmail.com>
2024-10-08 16:07:50 -0500

    feat: index.js items refactored

--- src/index.js
+++ src/index.js
@@ -26,38 +26,38 @@ const items = [
         content: "Supplies nutrition to recover a small amount of stamina.",
         img: "./../assets/items/hd.gif"
     },
-    // {
-    //     title: "Lighter",
-    //     content: "Metal lighter. Looks like its flame is stronger than normal lighters.",
-    //     img: "./../assets/items/lighter.gif"
-    // },
-    // {
-    //     title: "First Aid Kit",
-    //     content: "A medical kit; increased healing properties.",
-    //     img: "./../assets/items/fak.gif"
-    // },
-    // {
-    //     title: "Flashlight",
-    //     content: "One touch on/off switch. In the chest pocket.",
-    //     img: "./../assets/items/ilghtitem.gif"
-    // },
-    // {
-    //     title: "Video Tape",
-    //     content: "Videotape I forgot at the hotel three years ago.",
-    //     img: "./../assets/items/videotape.gif"
-    // },
+    {
+        title: "Lighter",
+        content: "Metal lighter. Looks like its flame is stronger than normal lighters.",
+        img: "./../assets/items/lighter.gif"
+    },
+    {
+        title: "First Aid Kit",
+        content: "A medical kit; increased healing properties.",
+        img: "./../assets/items/fak.gif"
+    },
+    {
+        title: "Flashlight",
+        content: "One touch on/off switch. In the chest pocket.",
+        img: "./../assets/items/ilghtitem.gif"
+    },
+    {
+        title: "Video Tape",
+        content: "Videotape I forgot at the hotel three years ago.",
+        img: "./../assets/items/videotape.gif"
+    },
 ];
 
-for (let i = 0; i < items.length; i++) {
+for (let i = 0; i < 5; i++) {
     const element = document.querySelector(`.inventory .row:nth-child(3) .card:nth-child(${i + 1}) .body .item`);
     if (element) {
-        element.id = `item-${i}`;
-        element.style.backgroundImage = `url(${items[i].img})`;
+        element.id = `item-${i + 2}`;
+        element.style.backgroundImage = `url(${items[i + 2].img})`;
     }
 }
 
-document.querySelector(`.inventory .row:nth-child(4) .title span`).innerText = items[4].title;  
-document.querySelector(`.inventory .row:nth-child(4) .content`).innerText = items[4].content; 
+document.querySelector(`.inventory .row:nth-child(4) .title span`).innerText = items[4].title;
+document.querySelector(`.inventory .row:nth-child(4) .content`).innerText = items[4].content;
 
 document.querySelector(`.inventory .row:nth-child(3) .card:nth-child(2) .body span svg`).style.backgroundImage = "url('assets/icons/arrow-left.svg')";
 document.querySelector(`.inventory .row:nth-child(3) .card:nth-child(4) .body span svg`).style.backgroundImage = `url('assets/icons/arrow-right.svg')`;
@@ -71,16 +71,20 @@ let i = items.length;
 
 previous.onclick = function () {
 
-    if(i == 0){
+    if (i == 0) {
         i = items.length
     }
-    console.log(items[i-1])
-    let prevId = parseInt(document.querySelector(`.inventory .row:nth-child(3) .card:nth-child(${i}) .body .item`).id.split('-')[1]);
+    let prevId = i;
 
     for (let j = 1; j <= items.length; j++) {
-        document.querySelector(`.inventory .row:nth-child(3) .card:nth-child(${j}) .body .item`).style.backgroundImage = `url(${items[prevId].img})`;
-        document.querySelector(`.inventory .row:nth-child(4) .title span`).innerText = items[prevId].title;  // Update title
-        document.querySelector(`.inventory .row:nth-child(4) .content`).innerText = items[prevId].content;  // Update content
+        if (j >= 3 && j <= 7) {
+            document.querySelector(`.inventory .row:nth-child(3) .card:nth-child(${j - 2}) .body .item`).style.backgroundImage = `url(${items[prevId].img})`; // Update cards
+        }
+        if (j == 5) {
+            document.querySelector(`.inventory .row:nth-child(2) .card:nth-child(2) .body .item`).style.backgroundImage = `url(${items[prevId].img})`; // Update selected
+            document.querySelector(`.inventory .row:nth-child(4) .title span`).innerText = items[prevId].title;  // Update title
+            document.querySelector(`.inventory .row:nth-child(4) .content`).innerText = items[prevId].content;  // Update content 
+        }
         prevId = (prevId === 0) ? items.length - 1 : prevId - 1; // Proper rotation
     }
 
@@ -88,13 +92,22 @@ previous.onclick = function () {
 };
 
 next.onclick = function () {
-    let nextId = parseInt(document.querySelector(`.inventory .row:nth-child(3) .card:nth-child(${i}) .body .item`).id.split('-')[1]);
+    let nextId = i - 1;
 
     for (let j = 1; j <= items.length; j++) {
-        document.querySelector(`.inventory .row:nth-child(3) .card:nth-child(${j}) .body .item`).style.backgroundImage = `url(${items[nextId].img})`;
-        document.querySelector(`.inventory .row:nth-child(4) .title span`).innerText = items[nextId].title;  // Update title
-        document.querySelector(`.inventory .row:nth-child(4) .content`).innerText = items[nextId].content;  // Update content
+
+        if (j >= 3 && j <= 7) {
+            document.querySelector(`.inventory .row:nth-child(3) .card:nth-child(${j - 2}) .body .item`).style.backgroundImage = `url(${items[nextId].img})`; // Update cards
+        }
+        if (j == 5) {
+            document.querySelector(`.inventory .row:nth-child(2) .card:nth-child(2) .body .item`).style.backgroundImage = `url(${items[nextId].img})`; // Update selected
+            document.querySelector(`.inventory .row:nth-child(4) .title span`).innerText = items[nextId].title;  // Update title
+            document.querySelector(`.inventory .row:nth-child(4) .content`).innerText = items[nextId].content;  // Update content 
+        }
+
         nextId = (nextId === items.length - 1) ? 0 : nextId + 1; // Proper rotation
+        console.log(nextId)
+
     }
     i++;
     if (i === items.length + 1) {
